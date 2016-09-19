e5301853ddbcb739330635c4f57976b6882947bf
Cixtor <cixtords@gmail.com>
2016-09-19 09:00:00 -0500

    Add command line options and usage information

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "flag"
+import (
+	"flag"
+	"fmt"
+)
 
 const service = "http://proxylist.hidemyass.com/"
 
@@ -9,6 +12,26 @@ var exportJSON = flag.Bool("json", false, "Print JSON encoded proxy server list"
 var exportCSV = flag.Bool("csv", false, "Print CSV encoded proxy server list")
 
 func main() {
+	flag.Usage = func() {
+		fmt.Println("ProxyList")
+		fmt.Println("http://cixtor.com/")
+		fmt.Println("https://github.com/cixtor/proxylist")
+		fmt.Println("https://en.wikipedia.org/wiki/Proxy_server")
+		fmt.Println(service)
+		fmt.Println()
+		fmt.Println("In computer networks, a proxy server is a server (a computer system or an\n" +
+			"application) that acts as an intermediary for requests from clients seeking\n" +
+			"resources from other servers. A client connects to the proxy server, requesting\n" +
+			"some service, such as a file, connection, web page, or other resource available\n" +
+			"from a different server and the proxy server evaluates the request as a way to\n" +
+			"simplify and control its complexity. Proxies were invented to add structure and\n" +
+			"encapsulation to distributed systems. Today, most proxies are web proxies,\n" +
+			"facilitating access to content on the World Wide Web and providing anonymity.\n")
+
+		fmt.Println("Usage:")
+		flag.PrintDefaults()
+	}
+
 	flag.Parse()
 
 	stream := htmlDocument(service)
