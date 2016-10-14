d7bff0a55094c5cf2b8e4ed7b1648ddc20651935
Cixtor <cixtords@gmail.com>
2016-10-14 09:00:00 -0500

    Modify padding and width of some columns in table
    Both the Speed and Connection columns in the table renderer display a
    percentage value, these numbers have better visibility if they are
    aligned to the right rather than the left as they are now. The Active
    column is also modified to increase the width of the text and the
    padding was moved to the right to align the text to the left.

--- export.go
+++ export.go
@@ -10,7 +10,7 @@ import (
 func printAsTable(lines []string) {
 	var count int
 
-	fmt.Println("## |     Alive | Speed | Conn | Anonimity | Protocol + Address + Port     | Country")
+	fmt.Println("## | Alive      | Speed | Conn | Anonimity | Protocol + Address + Port     | Country")
 	fmt.Println("--------------------------------------------------------------------------------------")
 
 	for _, line := range lines {
@@ -19,9 +19,9 @@ func printAsTable(lines []string) {
 
 		fmt.Printf("%02d | %s | %s | %s | %s | %s | %s\n",
 			count,
-			padleft(proxy.LastUpdate, 9),
-			padright(proxy.Speed, 5),
-			padright(proxy.Connection, 4),
+			padright(proxy.LastUpdate, 10),
+			padleft(proxy.Speed, 5),
+			padleft(proxy.Connection, 4),
 			padright(proxy.Anonimity, 9),
 			padright(fullhost(proxy), 29),
 			proxy.Country,
